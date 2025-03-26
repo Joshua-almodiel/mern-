c43762800f5a214450f552b6c6f105f822aa424f
Joshua Almodiel <joshuaalmodiel1133333@gmail.com>
2025-03-27 03:57:26 +0800

    updated

--- front/src/Components/Worker/Edit.jsx
+++ front/src/Components/Worker/Edit.jsx
@@ -179,7 +179,25 @@ const Edit = () => {
                     </div>
                 </form>
             </div>
-        ) : <div className="p-6 bg-gray-900 text-white text-center">Loading...</div>}</>
+        ) : <div className="p-8 text-center">
+        <svg
+          className="w-16 h-16 mx-auto text-gray-500"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1}
+            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <h3 className="mt-4 text-lg font-medium text-gray-300">
+          Loading Worker...
+        </h3>
+      </div>}</>
     );
 };
 
